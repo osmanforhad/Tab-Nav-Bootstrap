b047b2999e521c23ce05bbef32cf40dce6045eea
osmanforhad <osmanforhad93@gmail.com>
2018-11-15 01:10:34 +0600

    Bootstrap tab navigation

---
+++ Tab-Nav-Bootstrap/index.php
@@ -0,0 +1,53 @@
+<html>
+    <head>
+        <title>Boootstrap Tab Nav</title>
+        
+ <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+<link href="Style.css" rel="stylesheet" type="text/css">
+<!------ Include the above in your HEAD tag ---------->
+    </head>
+    <body>
+<div class="container">
+    <div class="page-header">
+        <h1>Bootstrap nav tabs.</h1>
+    </div>
+    
+</div
+<div class="container">
+    <div class="row">
+    	<div class="col-md-12">
+            <div class="panel with-nav-tabs panel-warning">
+                <div class="panel-heading">
+                        <ul class="nav nav-tabs">
+                            <li class="active"><a href="#tab1warning" data-toggle="tab">Tab 1</a></li>
+                            <li><a href="#tab2warning" data-toggle="tab">Tab 2</a></li>
+                            <li><a href="#tab3warning" data-toggle="tab">Tab 3</a></li>
+                           
+                        </ul>
+                </div>
+                <div class="panel-body">
+                    <div class="tab-content">
+                        <div class="tab-pane fade in active" id="tab1warning">Tab 1 body</div>
+                        <div class="tab-pane fade" id="tab2warning">Tab 2 body</div>
+                        <div class="tab-pane fade" id="tab3warning">Tab 3 body</div>
+                        <div class="tab-pane fade" id="tab4warning">Tab 4 body</div>
+                        <div class="tab-pane fade" id="tab5warning">Tab 5 body</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+	</div>
+</div>
+<br/> 
+    </body>
+</html>
+
